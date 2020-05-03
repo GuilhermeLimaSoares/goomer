12c30106c1d9e6d105c034700431528b227ea384
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-05-03 17:56:33 -0300

    HEADER: adicionando itens no header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import "./../header/header.scss";
 
 function Header() {
-        return(<div className="header">
+        return (<div className="header">
+                <nav className="navegation">
+                        <ul className="list">
+                                <li className="list__item"> <Link className="content" to="/home">Home</Link></li>
+                                <li className="list__item"> <Link className="content" to="/restaurantes">Restaurantes</Link></li>
+                                <li className="list__item"> <Link className="content" to="/sobre">Quem somos</Link></li>
+                        </ul>
+                </nav>
         </div>)
 
 }
